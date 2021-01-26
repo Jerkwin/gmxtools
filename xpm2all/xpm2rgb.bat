31247bbb20a478289ce92a8899da2d5643fd5202
jerkwin <jerkwin@gmail.com>
2021-01-25 23:10:20 -0500

    regular

--- xpm2all/xpm2rgb.bat
+++ xpm2all/xpm2rgb.bat
@@ -4,7 +4,7 @@ set File=%~n1
 
 bash xpm2all.bsh -rgb 8 %File%
 
-start /min D:\PicTool\View\i_view32.exe %File%~iv.xpm
+start /min D:\PicTool\iview-4.54\i_view64.exe %File%~iv.xpm
 
 sleep 1
 del /F /Q %File%~iv.xpm
